056468cc73c757881540dbcf32822c5e2e6b4b7c
Nick Miller <miller.nick.c@gmail.com>
2024-05-04 22:00:00 +0530

    add router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import './App.css';
 import FormComponent from '../FormComponent/FormComponent.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <FormComponent />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn to code in React right now
-        </a>
-        
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/form" element={<FormComponent />} />
+            <Route path="/" element={
+              <>
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn to code in React right now
+                </a>
+              </>
+            } />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
